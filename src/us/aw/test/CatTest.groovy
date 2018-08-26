4c0919ccd3eeede22866a596346b61a1ee773490
awashington267 <awashington7@my.wctc.edu>
2018-08-26 12:22:48 -0500

    Updated

---
+++ src/us/aw/test/CatTest.groovy
@@ -0,0 +1,22 @@
+package us.aw.test
+
+class CatTest extends groovy.util.GroovyTestCase {
+    void setUp() {
+        super.setUp()
+    }
+
+    void tearDown() {
+    }
+
+    void testGetMousesKilled() {
+    }
+
+    void testAddMouse() {
+    }
+
+    void testTalk() {
+    }
+
+    void testToString() {
+    }
+}
